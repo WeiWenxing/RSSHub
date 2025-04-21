4882aa2e88c2b8a121e2ce450f36a5e25986b7e6
weiwx <weiwx@mangdang.com>
2025-04-21 23:38:44 +0800

    fix vercel error

--- api/vercel.ts
+++ api/vercel.ts
@@ -1,11 +1,8 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import moduleAlias from 'module-alias';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-moduleAlias.addAlias('@', join(__dirname, '../lib'));
 
 import { setConfig } from '../lib/config.js';
 setConfig({
@@ -20,5 +17,6 @@ logger.info(`🎉 RSSHub is running! Cheers!`);
 logger.info('💖 Can you help keep this open source project alive? Please sponsor 👉 https://docs.rsshub.app/sponsor');
 
 export default handle(app);
+
 
 
